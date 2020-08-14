0cf5d1dcbfc18b6cd1ae59c108a77aacb2de6fd7
Jonelle Noelani Yacapin <jonelle_noelani@hotmail.com>
2020-08-14 03:59:53 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(hash)
   key = nil
   lowest_value = nil
   hash.each do |name, value|
-    if lowest value == nil || value < lowest_value  ## how can an integer be less than nil?
+    if lowest value == nil or value < lowest_value  ## how can an integer be less than nil?
     lowest_value = value
     key = name
   end
